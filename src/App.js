3f3db2b0879f95be9ef54a1d36be970c1aed6086
Johnny Choo <jchoo156@gmail.com>
2017-01-06 01:29:27 -0800

    refactor how user data is handled and displayed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import UserMeritRow from './UserMeritRow';
 import MeritOptions from './MeritOptions';
+import SelectedEmail from './SelectedEmail';
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class App extends Component {
     this.state = {
       selected: [],
       selectedCompany: 'sigmaEngineering',
+      selectedEmail: 'jchoo156@gmail.com',
       companyMerits: {
         sigmaEngineering: [
           { id: "x", title: "1000 Commits" },
@@ -23,35 +25,113 @@ class App extends Component {
       },
 
       rows: [
-        { 
-          email: 'jchoo156@gmail.com',
-          firstName: 'Johnny',
-          lastName: 'Choo',
-          merit: { type: 'sigmaEngineering', id: 'x' },
-          date: '12/01/2018',
-          expirationDate: '8/01/2022',
-          identificationCode: '123',
-          selected: false
+        {
+          'jchoo156@gmail.com': [
+            { 
+              email: 'jchoo156@gmail.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'sigmaEngineering', id: 'x' },
+              date: '12/01/2018',
+              expirationDate: '8/01/2022',
+              identificationCode: '123',
+              selected: false
+            },
+            { 
+              email: 'jchoo156@gmail.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'sigmaEngineering', id: 'y' },
+              date: '01/01/2017',
+              expirationDate: '02/01/2021',
+              identificationCode: 'abc',
+              selected: false
+            },
+            { 
+              email: 'jchoo156@gmail.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'sigmaEngineering', id: 'z' },
+              date: '3/01/2015',
+              expirationDate: '5/01/2019',
+              identificationCode: '1337',
+              selected: false
+            },
+            { 
+              email: 'jchoo156@gmail.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'superSmashBros', id: 'x' },
+              date: '4/01/2012',
+              expirationDate: '5/01/2019',
+              identificationCode: '1337',
+              selected: false
+            },
+            { 
+              email: 'jchoo156@gmail.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'superSmashBros', id: 'y' },
+              date: '11/01/2010',
+              expirationDate: '5/01/2016',
+              identificationCode: '1337',
+              selected: false
+            }
+          ]
         },
-        { 
-          email: 'jchoo156@gmail.com',
-          firstName: 'Johnny',
-          lastName: 'Choo',
-          merit: { type: 'sigmaEngineering', id: 'y' },
-          date: '01/01/2017',
-          expirationDate: '02/01/2021',
-          identificationCode: 'abc',
-          selected: false
-        },
-        { 
-          email: 'jchoo156@gmail.com',
-          firstName: 'Johnny',
-          lastName: 'Choo',
-          merit: { type: 'sigmaEngineering', id: 'z' },
-          date: '3/01/2015',
-          expirationDate: '5/01/2019',
-          identificationCode: '1337',
-          selected: false
+        {
+          'omer@sigma.com': [
+            { 
+              email: 'omer@sigma.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'sigmaEngineering', id: 'x' },
+              date: '12/01/2018',
+              expirationDate: '8/01/2022',
+              identificationCode: '123',
+              selected: false
+            },
+            { 
+              email: 'omer@sigma.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'sigmaEngineering', id: 'y' },
+              date: '01/01/2017',
+              expirationDate: '02/01/2021',
+              identificationCode: 'abc',
+              selected: false
+            },
+            { 
+              email: 'omer@sigma.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'sigmaEngineering', id: 'z' },
+              date: '3/01/2015',
+              expirationDate: '5/01/2019',
+              identificationCode: '1337',
+              selected: false
+            },
+            { 
+              email: 'omer@sigma.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'superSmashBros', id: 'x' },
+              date: '4/01/2012',
+              expirationDate: '5/01/2019',
+              identificationCode: '1337',
+              selected: false
+            },
+            { 
+              email: 'omer@sigma.com',
+              firstName: 'Johnny',
+              lastName: 'Choo',
+              merit: { type: 'superSmashBros', id: 'y' },
+              date: '11/01/2010',
+              expirationDate: '5/01/2016',
+              identificationCode: '1337',
+              selected: false
+            }
+          ]
         }
       ]
     }
@@ -61,6 +141,7 @@ class App extends Component {
     this.chooseOrganization = this.chooseOrganization.bind(this);
     this.removeMerits = this.removeMerits.bind(this);
     this.sendMerits = this.sendMerits.bind(this);
+    this.selectEmail = this.selectEmail.bind(this);
   };
 
   addMerit() {
@@ -117,12 +198,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>{this.state.rows.map((user, i) => user.selected.toString())}</h1>
-        <button onClick={this.removeMerits}>Remove</button>
-        <button onClick={this.sendMerits}>Send</button>
-        <MeritOptions chooseOrganizationProp={this.chooseOrganization} companyMeritsProp={Object.keys(this.state.companyMerits)} />
-        {this.state.rows.map((user, i) => (user.merit.type == this.state.selectedCompany) ? <UserMeritRow key={i} selectedProp={user.selected} selectProp={this.selectMerit} emailProp={user.email} firstNameProp={user.firstName} lastNameProp={user.lastName} meritIdProp={user.merit.id} meritProp={this.state.companyMerits[user.merit.type]} dateProp={user.date} expirationDateProp={user.expirationDate} identificationProp={user.identificationCode}/> : "")}
-        <button onClick={this.addMerit}>ADD</button>
+        {this.state.selectedEmail}
+        {this.state.rows.map((email,i) => (Object.keys(email) == this.state.selectedEmail) ? 
+          <div>
+            <button onClick={this.removeMerits}>Remove</button>
+            <button onClick={this.sendMerits}>Send</button>
+            <MeritOptions chooseOrganizationProp={this.chooseOrganization} companyMeritsProp={Object.keys(this.state.companyMerits)} />
+            {email[Object.keys(email)].map((user, i) => (user.merit.type == this.state.selectedCompany) ? <UserMeritRow key={i} selectedProp={user.selected} selectProp={this.selectMerit} emailProp={user.email} firstNameProp={user.firstName} lastNameProp={user.lastName} meritIdProp={user.merit.id} meritProp={this.state.companyMerits[user.merit.type]} dateProp={user.date} expirationDateProp={user.expirationDate} identificationProp={user.identificationCode}/> : "")}
+            <button onClick={this.addMerit}>ADD</button>
+          </div>
+         : "" )}
+        }
       </div>
     );
   }
---
+++ src/SelectedEmail.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SelectedEmail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <select>
+
+        </select>
+      </div>
+    );
+  }
+}
+
+export default SelectedEmail;
